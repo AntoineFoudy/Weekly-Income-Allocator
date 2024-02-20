4ac6e16ffa4d4b58d76cd459b56d301b6429e0f4
AntoineFoudy <145511347+AntoineFoudy@users.noreply.github.com>
2024-02-20 02:28:29 +0000

    Quick Improvment
    Now the left over from the difference of the spending money is now allocated into different accounts, leaving no sum unaccounted for

--- WeeklyIncome.java
+++ WeeklyIncome.java
@@ -16,18 +16,24 @@ public class WeeklyIncome {
         this.netIncome = Double.parseDouble(netIncomeString);
         this.spendAmountATM = Double.parseDouble(spendAmountATMString);
 
-        // Calculate how much money gets transfered to spending account using the fact
-        // that I can only spend 100€ a week
-        spendAmount = netIncome * .2;
-        if (spendAmount + spendAmountATM >= 100) {
-            spendAmount = 100 - spendAmountATM;
-        }
-
         // Calculating how the rest of the money gets distrabuted
         // Puts everything to the nearest 2 decimal places
         investAmount = Math.floor((netIncome * .5) * 100) / 100;
         currentAccount = Math.floor((netIncome * .25) * 100) / 100;
         saveAmount = Math.floor((netIncome * .05) * 100) / 100;
+
+        // Calculate how much money gets transfered to spending account using the fact
+        // That I can only spend 100€ a week
+        // It also adds the remainder to 2 diffrent accounts
+        spendAmount = netIncome * .2;
+        if (spendAmount + spendAmountATM >= 100) {
+            double getTo100SpendAmount = 100 - spendAmountATM;
+            double remainder = spendAmount - getTo100SpendAmount;
+            spendAmount = getTo100SpendAmount;
+            remainder = remainder * .5;
+            investAmount = investAmount + remainder;
+            currentAccount = currentAccount + remainder;
+        }
 
     }
 
