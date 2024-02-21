a7a0df863ead3fc32ac860531accc3d2fae9828b
AntoineFoudy <145511347+AntoineFoudy@users.noreply.github.com>
2024-02-21 02:39:46 +0000

    Improvements
    Just making the code look better and easier to understand

--- WeeklyIncome.java
+++ WeeklyIncome.java
@@ -17,10 +17,9 @@ public class WeeklyIncome {
         this.spendAmountATM = Double.parseDouble(spendAmountATMString);
 
         // Calculating how the rest of the money gets distrabuted
-        // Puts everything to the nearest 2 decimal places
-        investAmount = Math.floor((netIncome * .5) * 100) / 100;
-        currentAccount = Math.floor((netIncome * .25) * 100) / 100;
-        saveAmount = Math.floor((netIncome * .05) * 100) / 100;
+        investAmount = netIncome * .5;
+        currentAccount = netIncome * .25;
+        saveAmount = netIncome * .05;
 
         // Calculate how much money gets transfered to spending account using the fact
         // That I can only spend 100€ a week
@@ -34,6 +33,11 @@ public class WeeklyIncome {
             investAmount = investAmount + remainder;
             currentAccount = currentAccount + remainder;
         }
+
+        // Puts everything to the nearest 2 decimal places
+        investAmount = Math.floor(investAmount * 100) / 100;
+        currentAccount = Math.floor(currentAccount * 100) / 100;
+        saveAmount = Math.floor(saveAmount * 100) / 100;
 
     }
 
